833fadc770801797a8f2f5bcc3b4b68d67215a75
Mario Quilantan <mario.quilantan@semex.com.mx>
2021-10-29 07:51:18 -0500

    Update ConsoleHelper

--- Standard/Standard/ConsoleHelper.cs
+++ Standard/Standard/ConsoleHelper.cs
@@ -12,12 +12,48 @@ namespace maqdel.Infra
         private static readonly ILog _logger = LogManager.GetLogger(typeof(ConsoleHelper));
         
         /// <summary>
+        /// Set console TextColor
+        /// </summary>
+        /// <param name="TextColor"></param>
+        public static void SetConsoleColor(ConsoleColor TextColor)
+        {                 
+            _logger.Info("SetConsoleColor");
+            try
+            {
+                System.Console.ForegroundColor = TextColor;                
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("SetConsoleColor, Exception:", ex);
+            }
+        }
+
+        /// <summary>
+        /// Set console TextColor and BackgroundColor
+        /// </summary>
+        /// <param name="TextColor"></param>
+        /// <param name="BackgroundColor"></param>
+        public static void SetConsoleColor(ConsoleColor TextColor, ConsoleColor BackgroundColor)
+        {                 
+            _logger.Info("SetConsoleColor");
+            try
+            {
+                System.Console.ForegroundColor = TextColor;
+                System.Console.BackgroundColor = BackgroundColor;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("SetConsoleColor, Exception:", ex);
+            }
+        }
+        
+        /// <summary>
         /// Write text in a console position
         /// </summary>
         /// <param name="Col"></param>
         /// <param name="Row"></param>
         /// <param name="Text"></param>
-        public static void WriteIn(int Col, int Row, string Text) {                                    
+        public static void WriteIn(int Col, int Row, string Text) {
             _logger.Info("WriteIn");
             try
             {
@@ -31,7 +67,7 @@ namespace maqdel.Infra
         }
 
         /// <summary>
-        /// Write text in a console position with a color
+        /// Write text in a console position with a TextColor
         /// </summary>
         /// <param name="Col"></param>
         /// <param name="Row"></param>
@@ -42,8 +78,9 @@ namespace maqdel.Infra
             _logger.Info("WriteIn");
             try
             {
-                System.Console.ForegroundColor = TextColor;
-                System.Console.SetCursorPosition(Col, Row);            
+                SetConsoleColor(TextColor);
+                WriteIn(Col, Row, Text);
+                //System.Console.SetCursorPosition(Col, Row);            
                 System.Console.Write(Text);              
             }
             catch (Exception ex)
@@ -53,7 +90,92 @@ namespace maqdel.Infra
         }
 
         /// <summary>
+        /// Write text in a console position with a TextColor and BackgroundColor
+        /// </summary>
+        /// <param name="Col"></param>
+        /// <param name="Row"></param>
+        /// <param name="Text"></param>
+        /// <param name="TextColor"></param>
+        /// <param name="BackgroundColor"></param>
+        public static void WriteIn(int Col, int Row, string Text, ConsoleColor TextColor, ConsoleColor BackgroundColor)
+        {                 
+            _logger.Info("WriteIn");
+            try
+            {
+                SetConsoleColor(TextColor, BackgroundColor);
+                WriteIn(Col, Row, Text);
+                //System.Console.SetCursorPosition(Col, Row);
+                //System.Console.Write(Text);              
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("WriteIn, Exception:", ex);
+            }
+        }
+
+        /// <summary>
+        /// Clear screen and set TextColor
+        /// </summary>
+        /// <param name="TextColor"></param>
+        public static void Cls(ConsoleColor TextColor)
+        {                 
+            _logger.Info("Cls");
+            try
+            {                
+                SetConsoleColor(TextColor);
+                System.Console.Clear();                
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Cls, Exception:", ex);
+            }
+        }
+
+        /// <summary>
+        /// Clear screen and set TextColor and BackgroundColor
+        /// </summary>
+        /// <param name="TextColor"></param>
+        /// <param name="BackgroundColor"></param>
+        public static void Cls(ConsoleColor TextColor, ConsoleColor BackgroundColor)
+        {                 
+            _logger.Info("Cls");
+            try
+            {
+                SetConsoleColor(TextColor, BackgroundColor);
+                System.Console.Clear();                
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("Cls, Exception:", ex);
+            }
+        }
+
+
+        /// <summary>
         /// Write a column in the console
+        /// </summary>
+        /// <param name="Col"></param>
+        /// <param name="Row"></param>
+        /// <param name="ocurrs"></param>
+        public static void WriteCol(int Col, int Row, int ocurrs){
+            _logger.Info("WriteRow");
+            try
+            {                            
+                int prtLine = 1;
+                for (int row = Row; row <= (Row + ocurrs - 1); row++) {
+                    ConsoleHelper.WriteIn(Col, row, prtLine.ToString());
+                    prtLine++;
+                    if (prtLine == 10) prtLine = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("WriteRow, Exception:", ex);
+            }
+        }
+
+        /// <summary>
+        /// Write a column in the console and set TextColor
         /// </summary>
         /// <param name="Col"></param>
         /// <param name="Row"></param>
@@ -62,10 +184,37 @@ namespace maqdel.Infra
         public static void WriteCol(int Col, int Row, int ocurrs, ConsoleColor TextColor){
             _logger.Info("WriteRow");
             try
-            {                            
+            {                
+                SetConsoleColor(TextColor);            
                 int prtLine = 1;
                 for (int row = Row; row <= (Row + ocurrs - 1); row++) {
-                    ConsoleHelper.WriteIn(Col, row, prtLine.ToString(), TextColor);
+                    ConsoleHelper.WriteIn(Col, row, prtLine.ToString());
+                    prtLine++;
+                    if (prtLine == 10) prtLine = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("WriteRow, Exception:", ex);
+            }
+        }
+
+        /// <summary>
+        /// Write a column in the console and set TextColor and BackgroundColor
+        /// </summary>
+        /// <param name="Col"></param>
+        /// <param name="Row"></param>
+        /// <param name="ocurrs"></param>
+        /// <param name="TextColor"></param>
+        /// <param name="BackgroundColor"></param>
+        public static void WriteCol(int Col, int Row, int ocurrs, ConsoleColor TextColor, ConsoleColor BackgroundColor){
+            _logger.Info("WriteRow");
+            try
+            {                
+                SetConsoleColor(TextColor, BackgroundColor);            
+                int prtLine = 1;
+                for (int row = Row; row <= (Row + ocurrs - 1); row++) {
+                    ConsoleHelper.WriteIn(Col, row, prtLine.ToString());
                     prtLine++;
                     if (prtLine == 10) prtLine = 0;
                 }
@@ -88,9 +237,10 @@ namespace maqdel.Infra
             _logger.Info("WriteRow");
             try
             {                            
+                SetConsoleColor(TextColor);
                 int prtLine = 1;
                 for (int row = Row; row <= (Row + ocurrs - 1); row++) {
-                    ConsoleHelper.WriteIn(Col, row, Filler, TextColor);
+                    ConsoleHelper.WriteIn(Col, row, Filler);
                     prtLine++;
                     if (prtLine == 10) prtLine = 0;
                 }
@@ -107,8 +257,7 @@ namespace maqdel.Infra
         /// <param name="Col"></param>
         /// <param name="Row"></param>
         /// <param name="ocurrs"></param>
-        /// <param name="TextColor"></param>
-        public static void WriteRow(int Col, int Row, int ocurrs, ConsoleColor TextColor)
+        public static void WriteRow(int Col, int Row, int ocurrs)
         {
             _logger.Info("WriteCol");
             try
@@ -116,7 +265,7 @@ namespace maqdel.Infra
                 int prtLine = 1;
                 for (int col = Col; col <= (Col + ocurrs - 1); col++)
                 {
-                    ConsoleHelper.WriteIn(col, Row, prtLine.ToString(), TextColor);
+                    ConsoleHelper.WriteIn(col, Row, prtLine.ToString());
                     prtLine++;
                     if (prtLine == 10) prtLine = 0;
                 }                
@@ -128,7 +277,34 @@ namespace maqdel.Infra
         }
 
         /// <summary>
-        /// Write a Row in the console
+        /// Write a row in the console and set TextColor
+        /// </summary>
+        /// <param name="Col"></param>
+        /// <param name="Row"></param>
+        /// <param name="ocurrs"></param>
+        /// <param name="TextColor"></param>
+        public static void WriteRow(int Col, int Row, int ocurrs, ConsoleColor TextColor)
+        {
+            _logger.Info("WriteCol");
+            try
+            {
+                SetConsoleColor(TextColor);
+                int prtLine = 1;
+                for (int col = Col; col <= (Col + ocurrs - 1); col++)
+                {
+                    ConsoleHelper.WriteIn(col, Row, prtLine.ToString());
+                    prtLine++;
+                    if (prtLine == 10) prtLine = 0;
+                }                
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("WriteCol, Exception:", ex);
+            }
+        }
+
+        /// <summary>
+        /// Write a Row in the console and set TextColor
         /// </summary>
         /// <param name="Col"></param>
         /// <param name="Row"></param>
@@ -140,10 +316,40 @@ namespace maqdel.Infra
             _logger.Info("WriteCol");
             try
             {
+                SetConsoleColor(TextColor);
                 int prtLine = 1;
                 for (int col = Col; col <= (Col + ocurrs - 1); col++)
                 {
-                    ConsoleHelper.WriteIn(col, Row, Filler, TextColor);
+                    ConsoleHelper.WriteIn(col, Row, Filler);
+                    prtLine++;
+                    if (prtLine == 10) prtLine = 0;
+                }                
+            }
+            catch (Exception ex)
+            {
+                _logger.Error("WriteCol, Exception:", ex);
+            }
+        }
+
+        /// <summary>
+        /// Write a Row in the console and set TextColor and BackgroundColor
+        /// </summary>
+        /// <param name="Col"></param>
+        /// <param name="Row"></param>
+        /// <param name="ocurrs"></param>
+        /// <param name="Filler"></param>
+        /// <param name="TextColor"></param>
+        /// <param name="BackgroundColor"></param>
+        public static void WriteRow(int Col, int Row, int ocurrs, string Filler, ConsoleColor TextColor, ConsoleColor BackgroundColor)
+        {
+            _logger.Info("WriteCol");
+            try
+            {
+                SetConsoleColor(TextColor, BackgroundColor);
+                int prtLine = 1;
+                for (int col = Col; col <= (Col + ocurrs - 1); col++)
+                {
+                    ConsoleHelper.WriteIn(col, Row, Filler);
                     prtLine++;
                     if (prtLine == 10) prtLine = 0;
                 }                
