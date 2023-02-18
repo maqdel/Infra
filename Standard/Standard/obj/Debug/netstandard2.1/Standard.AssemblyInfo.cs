084e4c9cfdee4b1279d2c046870639306eefc31a
maqdel <58956137+maqdel@users.noreply.github.com>
2023-02-18 18:57:43 +0000

    create development branch

--- Standard/Standard/obj/Debug/netstandard2.1/Standard.AssemblyInfo.cs
+++ Standard/Standard/obj/Debug/netstandard2.1/Standard.AssemblyInfo.cs
@@ -12,13 +12,13 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("maqdel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("\r\n    Objects, Tools, and Helpers for .NET Development\r\n    \r\n    Compatible with" +
-    ": .NET 6.0/.NET 5.0/.NET Core 3.0/.NET Standard 2.1\r\n    ")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("2.1.0.13")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.1.0.13")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("\n    Objects, Tools, and Helpers for .NET Development\n    \n    Compatible with: ." +
+    "NET 6.0/.NET 5.0/.NET Core 3.0/.NET Standard 2.1\n    ")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("2.1.0.16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.1.0.16")]
 [assembly: System.Reflection.AssemblyProductAttribute("maqdel.Infra.Standard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("maqdel.Infra.Standard")]
-[assembly: System.Reflection.AssemblyVersionAttribute("2.1.0.13")]
+[assembly: System.Reflection.AssemblyVersionAttribute("2.1.0.16")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/maqdel/VSC_Infra_Standard_2_1")]
 [assembly: System.Resources.NeutralResourcesLanguageAttribute("en-US")]
 
